87940a709e29f0ac976122f4f69aa79fbf39c258
askvart <tur1amant@gmail.com>
2023-08-24 19:25:41 +0300

    game end

--- card/cards.go
+++ card/cards.go
@@ -1,5 +1,12 @@
 package card
 
+import (
+	"fmt"
+	"time"
+	"math/rand"
+	"strconv"
+)
+
 
 var ranks = []string{"2", "3", "4", "5","6","7","8","9","10","J","Q","K","A"}
 var suits = []rune{'\u2660','\u2661', '\u2662','\u2663'}
@@ -17,3 +24,62 @@ type Deck struct {
 	Cards []Card
 }
 
+func (h Hand) Value() int{
+	score := 0
+	haveAce := 0
+	for index:=0;index < len(h.Cards);index++{
+		if h.Cards[index].Rank != "A" && h.Cards[index].Rank != "K" && h.Cards[index].Rank != "Q" &&
+		   h.Cards[index].Rank != "J"{
+			intRank, _ := strconv.Atoi(h.Cards[index].Rank)
+			score += intRank
+		} else if h.Cards[index].Rank == "K" || h.Cards[index].Rank == "Q" || h.Cards[index].Rank == "J"{
+			score += 10
+		} else if h.Cards[index].Rank == "A"{
+			score += 11
+			haveAce += 1
+		}
+	}
+	if score > 21 && haveAce > 1{
+		score -= 10 * haveAce
+	} 
+	return score
+}
+
+func (h *Hand) AddCard(card Card){
+	h.Cards = append(h.Cards, card)
+}
+
+func (h *Hand) Display(){
+	for _,card := range h.Cards{
+		fmt.Print(card.Rank + card.Suit + "..")
+	}
+}
+
+
+func (deck *Deck) InitializeDeck() Deck{
+	for _, suit := range suits {
+		for _, rank := range ranks{
+			deck.Cards = append(deck.Cards, Card{Rank: rank, Suit: string(suit)})
+		} 
+	}
+	deck.Shuffle()
+	return *deck
+}
+
+
+func (deck *Deck) Shuffle(){
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(deck.Cards), func(i, j int) {deck.Cards[i], deck.Cards[j] = deck.Cards[j], deck.Cards[i]})
+}
+
+func (deck *Deck) DealCard() Card{
+	card := deck.Cards[0]
+	deck.Cards = deck.Cards[1:]
+	return card
+}
+
+func (deck *Deck) Display(){
+	for _, card := range deck.Cards{
+		fmt.Print(card.Rank + card.Suit + " | ")
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,73 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
 
+	"github.com/turamant/blackjack/card"
+)
 func main()  {
-	
+	gameOver := false
+	myDeck := card.Deck{}
+	myDeck.InitializeDeck()
+
+	kazinoHand := card.Hand{}
+	playerHand := card.Hand{}
+
+	for i:=1;i<=2;i++{
+		card := myDeck.DealCard()
+		kazinoHand.AddCard(card)
+		card = myDeck.DealCard()
+		playerHand.AddCard(card)
+	}
+	playerHand.Display()
+	fmt.Println("Ещё карту ? (y/n)")
+	reader := bufio.NewReader(os.Stdin)
+	res, _, _ := reader.ReadRune()
+	for {
+		if res != 'y'{
+			break
+		}
+		card := myDeck.DealCard()
+		playerHand.AddCard(card)
+		playerHand.Display()
+		if playerHand.Value() >21 {
+			fmt.Println("У вас больше 21. Вы проиграли!")
+			gameOver = true
+			break
+		}
+		fmt.Println("Ещё карту ? (y/n)")
+		reader := bufio.NewReader(os.Stdin)
+		res, _, _ = reader.ReadRune()
+	}
+	if !gameOver {
+		for {
+			if kazinoHand.Value() >21 {
+				fmt.Println("Казино больше 21. Вы выиграли")
+				gameOver = true
+				break
+			}
+			if kazinoHand.Value() < 17{
+				card := myDeck.DealCard()
+				kazinoHand.AddCard(card)
+			} else {
+				break
+			}
+		}
+	}
+	if !gameOver {
+		if playerHand.Value() > kazinoHand.Value(){
+			fmt.Println("Вы выиграли!")
+		}else if playerHand.Value() == kazinoHand.Value(){
+			fmt.Println("Ничья")
+		}else{
+			fmt.Println("Казино победило!")
+		}
+	}
+
+
+
+
+
 }
